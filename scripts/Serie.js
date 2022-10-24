6c28155cf1e41b9649a09624b87b1e3cd02363ef
camilo morillo <c.morillo@uniandes.edu.co>
2022-10-24 09:39:33 -0500

    first commit

---
+++ scripts/Serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, nombre, channel, seasons, description, url, image) {
+        this.id = id;
+        this.nombre = nombre;
+        this.channel = channel;
+        this.description = description;
+        this.seasons = seasons;
+        this.url = url;
+        this.image = image;
+    }
+    return Serie;
+}());
+export { Serie };
