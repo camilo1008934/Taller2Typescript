bbcae74ee87fc11c54ec734cec96ceba62642fe7
camilo morillo <c.morillo@uniandes.edu.co>
2022-10-24 12:19:01 -0500

    functionatilies done

--- main.ts
+++ main.ts
@@ -3,19 +3,26 @@ import {series} from "./data.js";
 
 const seriesTBody: HTMLElement = document.getElementById('series')!;
 const seasonsDiv: HTMLElement = document.getElementById('seasonsavg')!;
+const serieImage: HTMLElement = document.getElementById('serieImage')!;
+const serieTitle: HTMLElement = document.getElementById('serieTitle')!;
+const serieDescription: HTMLElement = document.getElementById('serieDescription')!;
+const serieUrl: HTMLElement = document.getElementById('serieUrl')!;
 
 renderSeriesInTable(series);
 averageSeasons(series);
-
 
 function renderSeriesInTable(series: Serie[]): void {
   series.forEach(c => {
     let trElement = document.createElement("tr");
     trElement.innerHTML = `<th scope="row">${c.id}</th>
-                           <td>${c.nombre}</td>
+                           <td><button class="name-btn" id="btn-${c.id}">${c.nombre}</button></td>
                            <td>${c.channel}</td>
                            <td>${c.seasons}</td>`;
     seriesTBody.appendChild(trElement);
+    let button: HTMLElement = document.getElementById("btn-"+c.id)!;
+    button.onclick = function(){
+      applySerieFilter(c);
+    };
   });
 }
 
@@ -30,4 +37,50 @@ function averageSeasons(series: Serie[]): void{
     let pElement = document.createElement("p");
     pElement.innerHTML=`Seasons Average: ${average}`;;
     seasonsDiv.appendChild(pElement);
+}
+
+function applySerieFilter(serie: Serie): void{
+  clearCard();
+
+  let image=document.createElement("img");
+  image.src="./images/"+serie.id+".jpg";
+  image.alt=serie.nombre;
+  image.width=350;
+  image.height=250;
+  serieImage.appendChild(image);
+
+  let title=document.createElement("h4");
+  title.textContent=serie.nombre;
+  serieTitle.appendChild(title);
+
+  let description=document.createElement("p");
+  description.textContent=serie.description;
+  serieDescription.appendChild(description);
+
+  let url=document.createElement("a");
+  url.href=serie.url;
+  url.className="btn btn-primary";
+  url.target="_black";
+  url.textContent="Visitar pagina";
+  serieUrl.appendChild(url);
+
+}
+
+function clearCard():void{
+  if (serieImage.lastChild != null){
+    serieImage.removeChild(serieImage.lastChild);
+  }
+
+  if (serieTitle.lastChild != null){
+    serieTitle.removeChild(serieTitle.lastChild);
+  }
+
+  if (serieDescription.lastChild != null){
+    serieDescription.removeChild(serieDescription.lastChild);
+  }
+
+  if (serieUrl.lastChild != null){
+    serieUrl.removeChild(serieUrl.lastChild);
+  }
+
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,21 @@
 import { series } from "./data.js";
 var seriesTBody = document.getElementById('series');
 var seasonsDiv = document.getElementById('seasonsavg');
+var serieImage = document.getElementById('serieImage');
+var serieTitle = document.getElementById('serieTitle');
+var serieDescription = document.getElementById('serieDescription');
+var serieUrl = document.getElementById('serieUrl');
 renderSeriesInTable(series);
 averageSeasons(series);
 function renderSeriesInTable(series) {
     series.forEach(function (c) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<th scope=\"row\">".concat(c.id, "</th>\n                           <td>").concat(c.nombre, "</td>\n                           <td>").concat(c.channel, "</td>\n                           <td>").concat(c.seasons, "</td>");
+        trElement.innerHTML = "<th scope=\"row\">".concat(c.id, "</th>\n                           <td><button class=\"name-btn\" id=\"btn-").concat(c.id, "\">").concat(c.nombre, "</button></td>\n                           <td>").concat(c.channel, "</td>\n                           <td>").concat(c.seasons, "</td>");
         seriesTBody.appendChild(trElement);
+        var button = document.getElementById("btn-" + c.id);
+        button.onclick = function () {
+            applySerieFilter(c);
+        };
     });
 }
 function averageSeasons(series) {
@@ -22,4 +30,39 @@ function averageSeasons(series) {
     pElement.innerHTML = "Seasons Average: ".concat(average);
     ;
     seasonsDiv.appendChild(pElement);
+}
+function applySerieFilter(serie) {
+    clearCard();
+    var image = document.createElement("img");
+    image.src = "./images/" + serie.id + ".jpg";
+    image.alt = serie.nombre;
+    image.width = 350;
+    image.height = 250;
+    serieImage.appendChild(image);
+    var title = document.createElement("h4");
+    title.textContent = serie.nombre;
+    serieTitle.appendChild(title);
+    var description = document.createElement("p");
+    description.textContent = serie.description;
+    serieDescription.appendChild(description);
+    var url = document.createElement("a");
+    url.href = serie.url;
+    url.className = "btn btn-primary";
+    url.target = "_black";
+    url.textContent = "Visitar pagina";
+    serieUrl.appendChild(url);
+}
+function clearCard() {
+    if (serieImage.lastChild != null) {
+        serieImage.removeChild(serieImage.lastChild);
+    }
+    if (serieTitle.lastChild != null) {
+        serieTitle.removeChild(serieTitle.lastChild);
+    }
+    if (serieDescription.lastChild != null) {
+        serieDescription.removeChild(serieDescription.lastChild);
+    }
+    if (serieUrl.lastChild != null) {
+        serieUrl.removeChild(serieUrl.lastChild);
+    }
 }
